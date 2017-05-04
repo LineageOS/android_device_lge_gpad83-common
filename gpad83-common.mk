3fa2013e83d3a7b18c892eeab05a9ecc1925da62
Pawin Vongmasa <pawin@google.com>
2017-05-03 21:54:00 -0500

    gpad83-common: Create media_profiles_V1_0.xml
    media_profiles_V1_0.xml is created from media_profiles.xml by replacing
    the dtd schema inside media_profiles.xml with an external reference to
    /system/etc/media_profiles_V1_0.dtd.
    
    Test: Compiles
    
    Bug: 36430270
    
    Change-Id: I2bdd38bc8e85cdb64c4729a6ebc843f5031c127c

--- gpad83-common.mk
+++ gpad83-common.mk
@@ -154,7 +154,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     device/lge/gpad83-common/configs/media_codecs.xml:system/etc/media_codecs.xml \
     device/lge/gpad83-common/configs/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
-    device/lge/gpad83-common/configs/media_profiles.xml:system/etc/media_profiles.xml \
+    device/lge/gpad83-common/configs/media_profiles_V1_0.xml:system/vendor/etc/media_profiles_V1_0.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
 
