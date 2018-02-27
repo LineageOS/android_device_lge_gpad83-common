bbf00823d6d1c2e8cf2b73a787587166d763916f
Luca Stefani <luca.stefani.ge1@gmail.com>
2018-02-26 19:42:45 -0600

    gpad83-common: Default OMX service to non-Treble
    Change-Id: If95974455c779ce0dd9598f8550dd8bb3018794c

--- gpad83-common.mk
+++ gpad83-common.mk
@@ -160,6 +160,9 @@ PRODUCT_PACKAGES += \
     libOmxVenc \
     libstagefrighthw
 
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.media.treble_omx=false
+
 # Power HAL
 PRODUCT_PACKAGES += \
     android.hardware.power@1.0-impl \
